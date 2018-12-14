825eb20737f603271206cfc76c757be5c5fb9ad9
PopoviciPaul <popovici.paul92@gmail.com>
2018-12-14 19:06:46 +0200

    Added the time module. Must now add the functionality which updates the displayed time for the stopwatch

--- watch.py
+++ watch.py
@@ -3,13 +3,17 @@ from tkinter import *
 from tkinter.ttk import *
 from tkinter import messagebox
 from tkinter import Menu
-
+import time
+import datetime
 
 # Define the application class where we will implement our widgets
 class Application(tk.Frame):
     def __init__(self, master):
         super(Application, self).__init__(master)
         self.grid()
+        self.current_time = StringVar() # the variable which displays the current time on the stopwatch
+        self.current_time.set("00:00")
+
         self.chosen_gui = "StopWatch"
         self.canvas = Canvas(self, width=800, height = 640, bg="gray") # IF YOU DO .PACK() HERE IT WILL RETURN NONE AND THEN YOU WILL HAVE PROBLEMS BECAUSE .PACK() RETURNS A 'NONE' TYPE OBJECT
         self.create_buttons()
@@ -29,16 +33,22 @@ class Application(tk.Frame):
         countdown_window = self.canvas.create_window(250, 0, anchor="nw", width=300, height=50, window=countdown_select)
         watch_window = self.canvas.create_window(550, 0, anchor="nw", width=250, height=50, window=watch_select)
 
+    def startCounter(self, start_from):
+        #start_from = type str: represents the time from which to re(start) the timer
+        start_time = time.time()
+        print(start_time)
+
+
     # This function updates the gui to correspond to the chosen app type: stopwatch, countdown or watch
     def update_tool_gui(self):
         if self.chosen_gui == "StopWatch":
             #print("Chosen functionality: %s" % self.chosen_gui)
-            digits = Label(self, text="00:00", background="lavender", anchor=CENTER)
+            digits = Label(self, textvariable=self.current_time, background="lavender", anchor=CENTER)
             digits.config(font=("Courier", 200))
 
             digits_window = self.canvas.create_window(0, 320, anchor="w", width=800, height=540, window=digits)
-            start_button = tk.Button(self, text="Start", bg="green2")
 
+            start_button = tk.Button(self, text="Start", bg="green2", command=lambda : self.startCounter(self.current_time.get()))
             stop_button = tk.Button(self, text="Stop/Reset", bg="red")
             start_button_window = self.canvas.create_window(0, 640, anchor="sw", width=400, height=50, window=start_button)
             stop_button_window = self.canvas.create_window(400, 640, anchor="sw", width=400, height=50, window=stop_button)
